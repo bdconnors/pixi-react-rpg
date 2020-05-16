afec46d4a6b5e31ae7416482524d9178ca4f699a
bdc5435 <bdc5435@rit.edu>
2020-05-15 21:02:35 -0400

    initial commit skeleton

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import App from './App.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById("root");
+
+ReactDOM.render(<App message={"Initial Commit Skeleton"}></App>,container);
